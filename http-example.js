5c8fb6bb7a70aaa7a858fd957db4b35468255c3e
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-22 20:09:29 +0100

    First commit

---
+++ http-example.js
@@ -0,0 +1,10 @@
+const { get }= require('node:https');
+
+get('https://www.google.com', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`Data chunk: ${chunk}`);
+    });
+    res.on('end', () => {
+        console.log('No more data in response.');
+    })
+});
